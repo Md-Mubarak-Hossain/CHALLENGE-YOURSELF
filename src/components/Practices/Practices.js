1102028ad46bfd3f5c7adac06a42763ecced67b0
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-10-11 14:52:36 +0600

    show correct answere

--- src/components/Practices/Practices.js
+++ src/components/Practices/Practices.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quizes from '../Quizes/Quizes';
-
+import './Practice.css'
 const Practices = () => {
     const practices = useLoaderData();
     const { data } = practices;
     const { id, questions } = data;
     console.log(questions);
+    console.log(id);
     return (
-        <div>
+        <div className='flex w-100'>
             <p>Practices</p>
-            {
-                questions.map(ques => <Quizes key={ques.id}
-                    ques={ques}></Quizes>)
-            }
+            <div>
+                {
+                    questions.map(ques => <Quizes key={ques.id}
+                        ques={ques}></Quizes>)
+                }
+            </div>
+
         </div>
     );
 };
